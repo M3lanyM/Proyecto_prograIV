9efae429ffd2858858781aa3dbdb22f1dedc398e
MartinJDUni <demiliano148@gmail.com>
2023-05-24 21:30:34 -0600

    calculator preview

---
+++ src/components/funtion/calculateprice.ts
@@ -0,0 +1,6 @@
+export function calculate(num1:number,num2:number,num3:number,route:string): number{
+    if(route== "Neily"){
+        return num1 + num2 + num3;
+    }
+    return 0;
+}
--- src/pages/indexPrice.tsx
+++ src/pages/indexPrice.tsx
@@ -1,29 +1,47 @@
+import { calculate } from "@/components/funtion/calculateprice";
 import Page_footer from "@/components/page_footer"
 import Page_header from "@/components/page_header"
+import React, {useState} from  "react";
 
-export default function indexReports() {
+export default function IndexPrice() {
+    const [input1,setInput1]=  useState<number>(0);
+    const [input2,setInput2]= useState<number>(0);
+    const [input3,setInput3]= useState<number>(0);
+    const [input4,setInput4]= useState("");
+    const [result, setResult]= useState<number>(0);
+
+    const handlerPrice = ()=>{
+        const Price = calculate(input1,input2,input3,input4);
+        setResult(Price);
+    }
+
     return (
         <>
             <header>
                 <Page_header />
             </header>
             <section className="m-4">
+                {/*Price*/}
                 <div className="grid grid-cols-2 gap-4">
                     <div>
                         <label form="weight">Ingrese el peso:</label>
-                        <input className="m-4" type="number" name="weight" placeholder="Kg" />
+                        <input className="m-4" type="number"
+                        value={input1} onChange={(e) => setInput1(parseInt(e.target.value))} placeholder="Kg" />
                     </div>
                     <div>
                         <label form="height">Ingrese la altura:</label>
-                        <input className="m-4" type="number" name="height" placeholder="H" />
+                        <input className="m-4" type="number"
+                        value={input2} onChange={(e) => setInput2(parseInt(e.target.value))} placeholder="H" />
                     </div>
                     <div>
                         <label form="width">Ingrese el ancho:</label>
-                        <input className="m-4" type="number" name="width" placeholder="W" />
+                        <input className="m-4" type="number"
+                        value={input3} onChange={(e) => setInput3(parseInt(e.target.value))} placeholder="W" />
                     </div>
                     <div>
                         <label form="Route">Seleccione la Ruta:</label>
-                        <select className="m-4" name="route">
+                        <select className="m-4"
+                        value={input4} onChange={(e) => setInput4(e.target.value)}>
                             <option value="SanJose">San Jose</option>
                             <option value="PasoCanoas">Paso Canoas</option>
                             <option value="Neily">Neily</option>
@@ -31,7 +49,8 @@ export default function indexReports() {
                     </div>
                 </div>
                 <div>
-                    <button className="Row">Calcular</button>
+                    <button className="Row" onClick={handlerPrice}>Calcular</button>
+                    <p>el precio es de: {result} colones</p>
                 </div>
             </section>
             <footer>
