588511785152bdfed2689e5512b1fafa06b4e681
Ashl3yV <127549843+Ashl3yV@users.noreply.github.com>
2023-05-25 03:02:45 -0600

    css modifications

--- src/pages/indexTracking.tsx
+++ src/pages/indexTracking.tsx
@@ -1,17 +1,48 @@
 import Page_header from '@/components/page_header';
-import React from 'react';
+import React, { useState } from "react";
+import Page_footer from '@/components/page_footer';
+
 
 const PackageTracking: React.FC = () => {
+    const [part, setPart] = useState(false);
+
+    const handlerPart = () => {
+        setPart(true);
+    }
     return (
         <>
             <header>
-                <Page_header></Page_header>
+                <Page_header />
             </header>
-            <div className="package-container">
-                <div className="circle" />
-                <div className="circleProcess" />
-                <div className="circleDelivered" />
-            </div>
+            <section>
+                <input className="code" type="text" placeholder="Código" />
+                <div>
+                    <button className="Rowtrack" onClick={handlerPart}>Rastrear</button>
+                    {part && (
+                        <section className="grid grid-cols-1 gap-4">
+                            <div className="circle" />
+                            <table className="table-reports">
+                                <thead>
+                                    <tr className="tr-reports">
+                                        <th className="th-reports">Fecha</th>
+                                        <th className="th-reports">Descripcion </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="tr-reports">
+                                        <td className="td-reports" data-label="Fecha">Dato 1</td>
+                                        <td className="td-reports" data-label="E/S">Dato 2</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </section>
+                    )}
+
+                </div>
+            </section>
+            <footer >
+                <Page_footer></Page_footer>
+            </footer>
         </>
     );
 };
