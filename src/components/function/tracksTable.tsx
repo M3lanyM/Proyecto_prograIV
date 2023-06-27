8ef6ab34c620060beb73c6cf20fb0f10efba8a9e
Ashl3yV <127549843+Ashl3yV@users.noreply.github.com>
2023-06-27 17:26:35 -0600

    Update tracksTable.tsx

---
+++ src/components/function/tracksTable.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "@/firebase/app";
+
+interface TabletracksProps {
+    selectedEncomiendaId: string;
+}
+
+interface EncomiendaData {
+
+    fecha: Date;
+    ruta: string;
+}
+
+const Tabletracks = ({ selectedEncomiendaId }: TabletracksProps) => {
+    const [encomiendaData, setEncomiendaData] = useState<EncomiendaData | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const app = initializeApp(firebaseConfig);
+            const database = getFirestore(app);
+
+            const encomiendaRef = doc(database, "encomienda", selectedEncomiendaId);
+            const encomiendaSnapshot = await getDoc(encomiendaRef);
+
+            if (encomiendaSnapshot.exists()) {
+                const encomiendaData = encomiendaSnapshot.data();
+                const rutaId = encomiendaData.ruta.id;
+
+                const rutaRef = doc(database, "ruta", rutaId);
+                const rutaSnapshot = await getDoc(rutaRef);
+
+                if (rutaSnapshot.exists()) {
+                    const rutaData = rutaSnapshot.data();
+                    const fecha = encomiendaData.fecha.toDate();
+                    const ruta = rutaData.nombre;
+
+                    setEncomiendaData({ fecha, ruta });
+                } else {
+                    setEncomiendaData(null);
+                }
+            } else {
+                setEncomiendaData(null);
+            }
+        };
+
+        if (selectedEncomiendaId) {
+            fetchData();
+        }
+    }, [selectedEncomiendaId]);
+
+    return (
+        <>
+            
+                <table className="table-track">
+                    <thead>
+                        <tr className="tr-track">
+                            <th className="th-track">Fecha</th>
+                            <th className="th-track ">Descripcion </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="tr-track">
+                            <td className="td-track" data-label="Fecha">Dato 1</td>
+                            <td className="td-track" data-label="E/S">Dato 2</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+        </>
+    );
+};
+
+export default Tabletracks;
