ab3384c0d900a940ead32a44cd74bbc29093765f
Melany Mendoza Vargas <melanymendoza2001@gmail.com>
2023-06-29 19:11:09 -0600

    update

--- src/pages/indexTracking.tsx
+++ src/pages/indexTracking.tsx
@@ -7,84 +7,101 @@ import { initializeApp } from "firebase/app";
 import firebaseConfig from "@/firebase/app";
 
 const PackageTracking: React.FC = () => {
-    const [selectedEncomiendaId, setselectedEncomiendaId] = useState<string>("");
-    const [part, setPart] = useState(false);
-    const [showError, setShowError] = useState(false);
-  
-    const handleRutaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setselectedEncomiendaId(event.target.value);
-    };
-  
-    const handlerPart = async () => {
-      const app = initializeApp(firebaseConfig);
-      const database = getFirestore(app);
-  
-      const encomiendaRef = doc(database, "encomienda", selectedEncomiendaId);
-      const encomiendaSnapshot = await getDoc(encomiendaRef);
-  
-      if (encomiendaSnapshot.exists()) {
-        setPart(true);
-        setShowError(false);
-      } else {
-        setShowError(true);
-      }
-    };
-  
-    return (
-      <>
-        <header>
-          <Page_header />
-        </header>
-        <section className="min-h-screen items-center">
-          <input
-            className="code"
-            type="text"
-            placeholder="Código"
-            value={selectedEncomiendaId}
-            onChange={(e) => setselectedEncomiendaId(e.target.value)}
-          />
-          <div>
-            <button className="Rowtrack" onClick={handlerPart}>
-              Rastrear
-            </button>
-            {showError ? (
-              <div className="error-popup">Error: El código no existe</div>
-            ) : part ? (
-              <section id="principalTrack" className="items-center">
-                <div className="downstairs">
-                  <div className="justify-center items-center">
-                    <div className="containerTrack">
-                      <div className="circleRegistered">
-                        <img src="/img/mercaderia.png" alt="Mercaderia" />
-                      </div>
-                      <label form="registered">Registrado</label>
+  const [selectedEncomiendaId, setselectedEncomiendaId] = useState<string>("");
+  const [part, setPart] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [encomiendaHora, setEncomiendaHora] = useState<Date | null>(null);
+
+  const handleRutaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setselectedEncomiendaId(event.target.value);
+  };
+
+  const handlerPart = async () => {
+    const app = initializeApp(firebaseConfig);
+    const database = getFirestore(app);
+
+    const encomiendaRef = doc(database, "encomienda", selectedEncomiendaId);
+    const encomiendaSnapshot = await getDoc(encomiendaRef);
+
+    if (encomiendaSnapshot.exists()) {
+      setPart(true);
+      setShowError(false);
+
+      const encomiendaData = encomiendaSnapshot.data();
+      const fecha = encomiendaData.fecha.toDate();
+      setEncomiendaHora(fecha);
+    } else {
+      setShowError(true);
+    }
+  };
+
+  const isTimePassed = (encomiendaHora: Date | null) => {
+    if (!encomiendaHora) {
+      return false;
+    }
+
+    const currentTimestamp = new Date().getTime();
+    const encomiendaTimestamp = encomiendaHora.getTime();
+    const fourHoursInMilliseconds = 4 * 60 * 60 * 1000; // 4 hours in milliseconds
+
+    return currentTimestamp - encomiendaTimestamp >= fourHoursInMilliseconds;
+  };
+
+  return (
+    <>
+      <header>
+        <Page_header />
+      </header>
+      <section className="min-h-screen items-center">
+        <input
+          className="code"
+          type="text"
+          placeholder="Código"
+          value={selectedEncomiendaId}
+          onChange={(e) => setselectedEncomiendaId(e.target.value)}
+        />
+        <div>
+          <button className="Rowtrack" onClick={handlerPart}>
+            Rastrear
+          </button>
+          {showError ? (
+            <div className="error-popup">Error: El código no existe</div>
+          ) : part ? (
+            <section id="principalTrack" className="items-center">
+              <div className="downstairs">
+                <div className="justify-center items-center">
+                  <div className="containerTrack">
+                    <div className={`circleRegistered ${isTimePassed(encomiendaHora) ? 'green' : ''}`}>
+                      <img src="/img/mercaderia.png" alt="Mercaderia" />
                     </div>
-                    <div className="containerTrack">
-                      <div className="circleProcess">
-                        <img src="/img/transito.png" alt="En Transito" />
-                      </div>
-                      <label form="process">En transito</label>
+                    <label form="registered">Registrado</label>
+                  </div>
+                  <div className="containerTrack">
+                    <div className={`circleProcess ${isTimePassed(encomiendaHora) ? 'green' : ''}`}>
+                      <img src="/img/transito.png" alt="En Transito" />
                     </div>
-                    <div className="containerTrack">
-                      <div className="circleDelivered">
-                        <img src="/img/entregado.png" alt="Entregado" />
-                      </div>
-                      <label form="delivered">Entregado</label>
+                    <label form="process">En transito</label>
+                  </div>
+                  <div className="containerTrack">
+                    <div className="circleDelivered">
+                      <img src="/img/entregado.png" alt="Entregado" />
                     </div>
+                    <label form="delivered">Entregado</label>
                   </div>
                 </div>
-                <section>
-                  <Tabletracks selectedEncomiendaId={selectedEncomiendaId} />
-                </section>
+              </div>
+              <section>
+                <Tabletracks selectedEncomiendaId={selectedEncomiendaId} />
               </section>
-            ) : null}
-          </div>
-        </section>
-        <footer>
-          <Page_footer />
-        </footer>
-      </>
-    );
-  };
-  
-  export default PackageTracking;
+            </section>
+          ) : null}
+        </div>
+      </section>
+      <footer>
+        <Page_footer />
+      </footer>
+    </>
+  );
+};
+
+export default PackageTracking;
